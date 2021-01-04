4e29c52c5dfa67d58b6bbfb494341af8fb4e8040
kyerkye <74213596+kyerkye@users.noreply.github.com>
2021-01-04 11:32:52 -0800

    Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./components/Home";
-import Resume from "./components/Resume";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import SinglePost from './components/SinglePost';
+import Post from './components/Post';
+import Project from './components/Project';
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
+
       <Switch>
-        <Route component={Home} path='/' />
-        <Route component={Resume} path='/resume' />
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/post/:slug' component={SinglePost} />
+        <Route path='/post' exact component={Post} />
+        <Route path='/project' component={Project} />
       </Switch>
     </BrowserRouter>
-  )
-
+  );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,23 @@
-import React from "react";
 
-export default function Home() {
-    return <h1>Home Page</h1>
-};
+import React from 'react';
+import image from '../images/home-bg.jpg';
+
+function Home() {
+  return (
+    <main>
+      <img
+        src={image}
+        alt='Monster Leaves'
+        className='absolute object-cover w-full h-full'
+      />
+
+      <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+        <h1 className='text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name'>
+          Hi I'm DerrekOW.
+        </h1>
+      </section>
+    </main>
+  );
+}
+
+export default Home;
