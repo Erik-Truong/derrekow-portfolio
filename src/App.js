c29589e0b000e230e705351abeda18a541155142
kyerkye <74213596+kyerkye@users.noreply.github.com>
2021-01-04 10:46:12 -0800

    React Routing and tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Resume from "./components/Resume";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Switch>
+        <Route component={Home} path='/' />
+        <Route component={Resume} path='/resume' />
+      </Switch>
+    </BrowserRouter>
+  )
+
 }
 
 export default App;
